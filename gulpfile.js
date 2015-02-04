2b82bc0ea66759c39cdc3a50731a7e106450ae40
Alexander Savin <box.for.postman@gmail.com>
2015-02-04 22:22:32 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     shell = require('gulp-shell'),
-    spawn = require('child_process').spawn;
+    spawn = require('child_process').spawn,
+    watch = require('gulp-watch'),
+    isInBuildProcess = false,
+    needReRun = false,
+    reRun = function () {
+        needReRun = false;
+        gulp.start('run');
+    };
 
 gulp.task('clean', shell.task([
     'node ./node_modules/.bin/bem make -m clean -v error'
@@ -36,11 +43,27 @@ gulp.task('run', ['build'], function () {
     } else {
         nodemonInstance.emit('restart');
     }
+    if (needReRun) { // it seems that some something changed during build process
+        setTimeout(reRun, 0); // lets run it again
+    } else {
+        isInBuildProcess = false; // end of build process
+    }
 });
 
 gulp.task('dev', ['run'], function () {
-    gulp.watch('desktop.blocks/**/*', ['run']);
-    gulp.watch('desktop.bundles/index/*.{yml,bemdecl.js}', ['run']);
+    watch([
+        'desktop.blocks/**/*',
+        'desktop.bundles/index/*.{yml,bemdecl.js}'
+    ], {
+        // usePolling: true // use this option if watch didn't catch your changes
+    }, function () {
+        if (isInBuildProcess) { // if something is changed during build process
+            needReRun = true; // we will need to run it again
+        } else {
+            isInBuildProcess = true; // build process started
+            gulp.start('run');
+        }
+    });
 });
 
 gulp.task('default', ['dev']);
